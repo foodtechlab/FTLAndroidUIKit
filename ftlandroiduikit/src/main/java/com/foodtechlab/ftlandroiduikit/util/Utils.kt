feed76c6a0d5db02b598eaa30bcfb87aa54dccfd
Umalt <isakkhazhiev@gmail.com>
2020-05-08 19:13:21 +0400

    - Add TopSnackbar

--- ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/util/Utils.kt
+++ ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/util/Utils.kt
@@ -2,6 +2,10 @@ package com.foodtechlab.ftlandroiduikit.util
 
 import android.content.Context
 import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 
 /**
  * Created by Umalt on 07.05.2020
@@ -21,4 +25,33 @@ fun Context.spToPx(sp: Float): Float {
         sp,
         resources.displayMetrics
     )
+}
+
+internal fun View?.findSuitableParent(): ViewGroup? {
+    var view = this
+    var fallback: ViewGroup? = null
+    do {
+        if (view is CoordinatorLayout) {
+            // We've found a CoordinatorLayout, use it
+            return view
+        } else if (view is FrameLayout) {
+            if (view.id == android.R.id.content) {
+                // If we've hit the decor content view, then we didn't find a CoL in the
+                // hierarchy, so use it.
+                return view
+            } else {
+                // It's not the content view but we'll use it as our fallback
+                fallback = view
+            }
+        }
+
+        if (view != null) {
+            // Else, we will loop and crawl up the view hierarchy and try to find a parent
+            val parent = view.parent
+            view = if (parent is View) parent else null
+        }
+    } while (view != null)
+
+    // If we reach here then we didn't find a CoL or a suitable content view so we'll fallback
+    return fallback
 }
