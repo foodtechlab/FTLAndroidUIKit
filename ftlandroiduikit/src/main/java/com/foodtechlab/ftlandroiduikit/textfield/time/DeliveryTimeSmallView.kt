822d9a194a5ef8ec88a5a38ba8d437b5f51d04e2
Umalt <isakkhazhiev@gmail.com>
2020-06-01 06:04:16 +0400

    Add DeliveryTimeSmallView Add DeliveryTimeLargeView

---
+++ ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/textfield/time/DeliveryTimeSmallView.kt
@@ -0,0 +1,15 @@
+package com.foodtechlab.ftlandroiduikit.textfield.time
+
+import android.content.Context
+import android.util.AttributeSet
+
+class DeliveryTimeSmallView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) :
+    DeliveryTimeView(context, attrs, defStyleAttr) {
+
+    init {
+    }
+}
---
+++ ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/textfield/time/helper/Icon.kt
@@ -0,0 +1,11 @@
+package com.foodtechlab.ftlandroiduikit.textfield.time.helper
+
+import android.graphics.drawable.Drawable
+
+data class Icon(
+    var width: Float,
+    var height: Float,
+    var marginEnd: Float,
+    var marginStart: Float,
+    var drawable: Drawable?
+)
