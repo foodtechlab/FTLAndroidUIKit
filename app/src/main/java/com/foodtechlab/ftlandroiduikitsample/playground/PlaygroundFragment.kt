ac3a51da03bbd7e0ff436917bc28a0480d5828c5
Lina Podbolotova <developer.kaczmarek@yandex.ru>
2021-05-06 16:58:09 +0300

    изменен width для FTLButton

--- app/src/main/java/com/foodtechlab/ftlandroiduikitsample/playground/PlaygroundFragment.kt
+++ app/src/main/java/com/foodtechlab/ftlandroiduikitsample/playground/PlaygroundFragment.kt
@@ -1,16 +1,50 @@
 package com.foodtechlab.ftlandroiduikitsample.playground
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.foodtechlab.ftlandroiduikit.button.FTLButton
 import com.foodtechlab.ftlandroiduikitsample.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class PlaygroundFragment : Fragment() {
+class PlaygroundFragment : Fragment(), View.OnClickListener, CoroutineScope {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main
+
+    lateinit var bSettings: FTLButton
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         return inflater.inflate(R.layout.fragment_playground, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bSettings = view.findViewById(R.id.btn_settings)
+        bSettings.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_settings -> {
+                launch {
+                    bSettings.setProgressVisibility(true)
+                    delay(5000)
+                    bSettings.setProgressVisibility(false)
+                }
+            }
+        }
     }
 
     companion object {
