0f752f435da14b568bdbf7f44caa854573d13036
Angelina Podbolotova <developer.kaczmarek@yandex.ru>
2021-07-20 08:55:57 +0300

    FUIDROID-24: Переработка компонента FTLTagView

--- app/src/main/java/com/foodtechlab/ftlandroiduikitsample/playground/PlaygroundFragment.kt
+++ app/src/main/java/com/foodtechlab/ftlandroiduikitsample/playground/PlaygroundFragment.kt
@@ -1,27 +1,16 @@
 package com.foodtechlab.ftlandroiduikitsample.playground
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.foodtechlab.ftlandroiduikit.common.NetworkConnectivityState
-import com.foodtechlab.ftlandroiduikit.tab.FTLTabLayout
-import com.foodtechlab.ftlandroiduikit.util.ThemeManager
 import com.foodtechlab.ftlandroiduikitsample.R
-import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_playground.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 class PlaygroundFragment : Fragment() {
+    var isVisibleBorder = false
+    var isVisibleBackground = true
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,12 +23,15 @@ class PlaygroundFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tg_test2.updateTrackColorTheme(
-            when(ThemeManager.theme) {
-                ThemeManager.Theme.LIGHT -> R.color.TimerNegativeLight
-                else -> R.color.TimerNegativeDark
-            }
-        )
+        btn_border.setOnClickListener {
+            isVisibleBorder = !isVisibleBorder
+            tg_test.shouldVisibleBorder = isVisibleBorder
+        }
+
+        btn_background.setOnClickListener {
+            isVisibleBackground = !isVisibleBackground
+            tg_test.shouldVisibleBackground = isVisibleBackground
+        }
     }
 
     companion object {
--- ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/textfield/FTLTagView.kt
+++ ftlandroiduikit/src/main/java/com/foodtechlab/ftlandroiduikit/textfield/FTLTagView.kt
@@ -4,73 +4,155 @@ import android.content.Context
 import android.text.TextUtils
 import android.util.AttributeSet
 import android.util.TypedValue
+import androidx.annotation.ColorRes
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.content.withStyledAttributes
+import androidx.core.view.updatePadding
 import com.foodtechlab.ftlandroiduikit.R
 import com.foodtechlab.ftlandroiduikit.util.ThemeManager
 import com.foodtechlab.ftlandroiduikit.util.dpToPx
-import com.google.android.material.chip.Chip
+import com.foodtechlab.ftlandroiduikit.util.dpToPxInt
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
+
 
 class FTLTagView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
-) : Chip(context, attrs, defStyleAttr), ThemeManager.ThemeChangedListener {
-    var tagText: String? = null
+) : AppCompatTextView(context, attrs, defStyleAttr), ThemeManager.ThemeChangedListener {
+    var shouldVisibleBackground = true
         set(value) {
             field = value
-            text = field
+            updateBackgroundColorTheme(tagBackgroundColor)
+            background = shapeDrawable
         }
 
-    var tagBackgroundColor = ContextCompat.getColorStateList(context, R.color.TagBackgroundLight)
+    var shouldVisibleBorder = false
         set(value) {
             field = value
-            chipBackgroundColor = field
+            updateBorderColorTheme(tagBorderColor)
+            background = shapeDrawable
         }
 
-    var tagBorderColor = ContextCompat.getColorStateList(context, R.color.TagBorderLight)
-        set(value) {
-            field = value
-            chipStrokeColor = field
-        }
+    var tag: String? = null
+    set(value) {
+        field = value
+        text = field
+    }
 
-    var tagTextColor = ContextCompat.getColorStateList(context, R.color.TextOnColorAdditionalLight)
-        set(value) {
-            field = value
-            setTextColor(field)
-        }
+    @ColorRes
+    private var tagBackgroundColor = -1
+
+    @ColorRes
+    private var tagBorderColor = -1
+
+    @ColorRes
+    private var tagTextColor = -1
+
+    private val shapeAppearanceModel = ShapeAppearanceModel()
+        .toBuilder()
+        .setAllCorners(CornerFamily.ROUNDED, context.dpToPx(CORNERS))
+        .build()
+
+    private val shapeDrawable = MaterialShapeDrawable(shapeAppearanceModel)
 
     init {
         context.withStyledAttributes(attrs, R.styleable.FTLTagView) {
-            tagText = getString(R.styleable.FTLTagView_android_text)
+            tag = getString(R.styleable.FTLTagView_android_text)
         }
-
-        setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE)
-        typeface = ResourcesCompat.getFont(context, R.font.roboto_regular)
-        chipStrokeWidth = context.dpToPx(STROKE_WIDTH)
-        isSingleLine = true
         ellipsize = TextUtils.TruncateAt.END
+        isSingleLine = true
+        setTextSize(TypedValue.COMPLEX_UNIT_SP, TEXT_SIZE)
+        lineHeight = context.dpToPxInt(20f)
+        typeface = ResourcesCompat.getFont(context, R.font.roboto_regular)
+
+        val verticalPaddings = context.dpToPxInt(VERTICAL_PADDING)
+        val horizontalPaddings = context.dpToPxInt(HORIZONTAL_PADDING)
+        updatePadding(
+            horizontalPaddings,
+            verticalPaddings,
+            horizontalPaddings,
+            verticalPaddings
+        )
+
         onThemeChanged(ThemeManager.theme)
-        chipEndPadding = context.dpToPx(HORIZONTAL_PADDING)
-        chipStartPadding = context.dpToPx(HORIZONTAL_PADDING)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        ThemeManager.addListener(this)
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        ThemeManager.removeListener(this)
     }
 
     override fun onThemeChanged(theme: ThemeManager.Theme) {
-        tagBackgroundColor = ContextCompat.getColorStateList(
-            context,
-            theme.ftlTagViewTheme.backgroundColor
-        )
-        tagBorderColor = ContextCompat.getColorStateList(context,  theme.ftlTagViewTheme.borderColor)
-        tagTextColor = ContextCompat.getColorStateList(
-            context,
-            theme.ftlTagViewTheme.textColor
+        updateTextColorTheme(tagTextColor)
+        updateBackgroundColorTheme(tagBackgroundColor)
+        updateBorderColorTheme(tagBorderColor)
+        background = shapeDrawable
+    }
+
+    fun updateTextColorTheme(@ColorRes colorRes: Int) {
+        tagTextColor = colorRes
+        setTextColor(
+            ContextCompat.getColorStateList(
+                context,
+                if (tagBorderColor != -1)
+                    tagTextColor
+                else
+                    ThemeManager.theme.ftlTagViewTheme.textColor
+            )
         )
     }
 
+    fun updateBackgroundColorTheme(@ColorRes colorRes: Int) {
+        tagBackgroundColor = colorRes
+        if (shouldVisibleBackground) {
+            shapeDrawable.fillColor = ContextCompat.getColorStateList(
+                context,
+                if (tagBackgroundColor != -1)
+                    tagBackgroundColor
+                else
+                    ThemeManager.theme.ftlTagViewTheme.backgroundColor
+            )
+        } else {
+            shapeDrawable.fillColor = ContextCompat.getColorStateList(
+                context,
+                android.R.color.transparent
+            )
+        }
+    }
+
+    fun updateBorderColorTheme(@ColorRes colorRes: Int) {
+        tagBorderColor = colorRes
+        if (shouldVisibleBorder) {
+            shapeDrawable.setStroke(
+                context.dpToPx(STROKE_WIDTH),
+                ContextCompat.getColor(
+                    context,
+                    if (tagBorderColor != -1)
+                        tagBorderColor
+                    else
+                        ThemeManager.theme.ftlTagViewTheme.borderColor
+                )
+            )
+        } else {
+            shapeDrawable.strokeWidth = 0F
+        }
+    }
+
     companion object {
-      const val STROKE_WIDTH = 1F
-      const val TEXT_SIZE = 14F
-      const val HORIZONTAL_PADDING = 8F
+        private const val TEXT_SIZE = 14F
+        private const val HORIZONTAL_PADDING = 8F
+        private const val VERTICAL_PADDING = 2F
+        private const val CORNERS = 16F
+        private const val STROKE_WIDTH = 1F
     }
 }
